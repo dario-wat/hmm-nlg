263924cfa2070c3683be1ac5871a69df59901da7
adhaka <akash.uee@gmail.com>
2014-10-21 13:15:48 +0200

    adding hmm file

--- hmm.py
+++ hmm.py
@@ -1,6 +1,8 @@
 
 import nltk.tag.hmm as hmm
 import nltk
+import sys
+from nltk.util import unique_list
 
 import random
 from sys import argv
@@ -8,19 +10,43 @@ from sys import argv
 
 s = """I am trying to use stupid hmm to generate a random sentence. This random sentence
                 is supposed to be very good, good"""
-with open(argv[1]) as f:
-        s = f.read()
 
-print len(s)
-s = s[0:1000]
-#print len(s)
+try:
+    f= open(sys.argv[1])
+except IndexError:
+    f = open('rawcorpus/andersen.txt')
+
+s= f.read()
+f.close()
+
+s = s[0:3000]
+
+# supervised training
+
+corpus = nltk.corpus.brown.tagged_sents(categories='news')[:3000]
+
+states = unique_list(tag for sent in corpus for (word,tag) in sent)
+symbols = unique_list(word for sent in corpus for (word,tag) in sent)
+
 sentences = nltk.sent_tokenize(s)
+
+tag_set = unique_list(tag for sent in corpus for (word,tag) in sent)
 seq = [map(lambda x:(x,''), ss.split()) for ss in sentences]
 #seq = [word for sent in sentences for word in nltk.word_tokenize(sent)]
-symbols = list(set([ss[0] for sss in seq for ss in sss]))
-print len(symbols)
-print len([word for sent in sentences for word in nltk.word_tokenize(sent)])
-states = symbols
+
+# unsupervised training
+
+# symbols = list(set([ss[0] for sss in seq for ss in sss]))
+# states = range(20)
+
+#states = symbols
 trainer = nltk.tag.hmm.HiddenMarkovModelTrainer(states=states,symbols=symbols)
-m = trainer.train_unsupervised(seq, max_iterations=30)
-print ' '.join(map(lambda (x, _): x, m.random_sample(random.Random(),30)))
+
+hmm = trainer.train_supervised(corpus)
+# m = trainer.train_unsupervisedseq, max_iterations=30)
+
+# print ' '.join(map(lambda (x, _): x, hmm.random_sample(random.Random(),50)))
+print ' '.join(map(lambda (x, _): x, hmm.random_sample(random.Random(),30)))
+
+
+class HmmWrapper:
